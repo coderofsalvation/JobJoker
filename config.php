e38b897d52466eb3d4cd12536024b2c49f29326e
Leon <lk@billypds.com>
2014-03-09 19:55:41 +0100

    modified config

--- config.php
+++ config.php
@@ -1,17 +1,27 @@
 <?php
-    $api = "http://localhost/diogo/jobjoker";
-    $php_command = "php";
-    $kill_command = "kill";
 
-    $mysql = false;
-    $mysql_host = "localhost";
-    $mysql_database = "jobjoker";
-    $mysql_user = "jobjoker";
-    $mysql_password = "jobjoker";
+$config = array(
+  'api'                        => "http://localhost/JobJoker",
+  'php_command'                => "/usr/bin/php",
+  'kill_command'               => "kill",
 
-    $auth = false;
-    $user = 'jobjoker';
-    $password = 'jobjoker';
-    $dont_restart_job_when_done = false; //  allows to (re)starting jobs when their status is 'done'/'error'/'stop'
+  'mysql'                      => true,
+  'mysql_host'                 => "localhost",
+  'mysql_database'             => "jobjoker",
+  'mysql_user'                 => "",
+  'mysql_password'             => "",
+
+  'auth'                       => false,
+  'user'                       => 'jobjoker',
+  'password'                   => 'jobjoker',
+
+  'dont_restart_job_when_done' => false,         // allows to (re)starting jobs when their status is 'done'/'error'/'stop'
+
+  'maintainerEmails'           => array("my@email.com"), // receive emails when things go wrong
+  'maintainerEmailFrom'        => "noreply@worker.com",
+  'maintainerEmailFromName'    => "JobJoker",
+);
+extract($config);
+$config = (object)$config;
 
 ?>
